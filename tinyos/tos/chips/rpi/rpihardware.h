bd63542ac81a00cc0a3eb0da736bbc8f8c899480
Andrew Robinson <andrew.robinson@gmail.com>
2013-01-20 12:14:10 -0500

    Added new atomic section code.
    We now use a mutex to ensure the atomicity of all TinyOS atomic sections.
    Of note is how we handle recursive locking. In traditional TinyOS
    enable/disable interrupt code it would return a 1 or 0 indicating whether
    interrupts were enabled or disabled. This was okay because of the nature
    of the way interrupts execute ensured that they would always run to
    completion.
    
    Instead we choose to use a recurisve lock. There's some race conditions
    that can crop up with the way linux does preemption that we needed to
    solve.

--- tinyos/tos/chips/rpi/rpihardware.h
+++ tinyos/tos/chips/rpi/rpihardware.h
@@ -2,21 +2,42 @@
 #ifndef __DEBIAN_HARDWARE_H__
 #define __DEBIAN_HARDWARE_H__
 
+#include <pthread.h>
+
 typedef uint8_t mcu_power_t;
 typedef bool __nesc_atomic_t;
+
+pthread_mutex_t atomic_lock;
+pthread_mutexattr_t mta;
+bool is_init = FALSE;
 
 __nesc_atomic_t __nesc_atomic_start(void);
 void __nesc_atomic_end(__nesc_atomic_t reenable_interrupts);
 
+// NOTE: In this code we're checking if the mutex is initialized many
+// times a second. It might be better to factor this out into a boot-time
+// initialization routine.
+// TODO: Refactor the checks for init out.
 void __nesc_disable_interrupt(void) @safe()
 {
- // dint();
-  //nop();
+	if (!is_init) {
+		pthread_mutexattr_init(&mta);
+		pthread_mutexattr_settype(&mta, PTHREAD_MUTEX_RECURSIVE);
+		pthread_mutex_init(&atomic_lock, &mta);
+	}
+
+	pthread_mutex_lock(&atomic_lock);
 }
 
 void __nesc_enable_interrupt(void) @safe()
 {
- // eint();
+	if (!is_init) {
+		pthread_mutexattr_init(&mta);
+		pthread_mutexattr_settype(&mta, PTHREAD_MUTEX_RECURSIVE);
+		pthread_mutex_init(&atomic_lock, &mta);
+	}
+
+	pthread_mutex_unlock(&atomic_lock);
 }
 
 /* @spontaneous() functions should not be included when NESC_BUILD_BINARY
@@ -25,17 +46,13 @@ void __nesc_enable_interrupt(void) @safe()
    though. */
 __nesc_atomic_t __nesc_atomic_start(void) @spontaneous() @safe()
 {
-//  __nesc_atomic_t result = ((READ_SR & SR_GIE) != 0);
-//  __nesc_disable_interrupt();
-//  asm volatile("" : : : "memory"); /* ensure atomic section effect visibility */
-  return 0;
+  __nesc_disable_interrupt();
+  return 1;
 }
 
 void __nesc_atomic_end(__nesc_atomic_t reenable_interrupts) @spontaneous() @safe()
 {
-//  asm volatile("" : : : "memory"); /* ensure atomic section effect visibility */
-//  if( reenable_interrupts )
-//    __nesc_enable_interrupt();
+  __nesc_enable_interrupt();
 }
 
 
