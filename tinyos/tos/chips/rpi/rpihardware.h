4a60720fc977a22e924feba695666c588201e939
Brad Campbell <bradjc@umich.edu>
2013-02-22 18:17:22 -0500

    Hopefully corrected the interrupt en/disable code
    There was still a race condition when interrupts were being disabled,
    and it was the classic "check before set" problem. That should be fixed
    and I removed the nesc warnings.

--- tinyos/tos/chips/rpi/rpihardware.h
+++ tinyos/tos/chips/rpi/rpihardware.h
@@ -1,4 +1,3 @@
-
 #ifndef __DEBIAN_HARDWARE_H__
 #define __DEBIAN_HARDWARE_H__
 
@@ -7,43 +6,50 @@
 typedef uint8_t mcu_power_t;
 typedef bool __nesc_atomic_t;
 
-sigset_t global_sig;
-bool interrupts_enabled = TRUE;
+// We can safely use norace with these variables. There are only accessed when
+// interrupts are disabled, but the nesc compiler doesn't know that.
+norace bool interupts_enabled = TRUE;
+norace sigset_t global_sigmask;
 
 __nesc_atomic_t __nesc_atomic_start(void);
 void __nesc_atomic_end(__nesc_atomic_t reenable_interrupts);
 
 void __nesc_disable_interrupt(void) @safe() {
   sigset_t all;
+  sigset_t temp_sigmask;
 
-  if (!interrupts_enabled) return;
-
+  // Unconditionally block all signals when this function is called. This
+  // simulates a microcontroller where you can disable and re-disable global
+  // interrupts anytime.
   sigfillset(&all);
-  sigprocmask(SIG_BLOCK, &all, &global_sig);
-  interrupts_enabled = FALSE;
+  sigprocmask(SIG_BLOCK, &all, &temp_sigmask);
+
+  // Now that interrupts are disabled, check to see if they were already
+  // disabled or, if not, we need to save the previous mask in order to use
+  // it to re-enable interrupts.
+  if (interupts_enabled) {
+    global_sigmask = temp_sigmask;
+    interupts_enabled = FALSE;
+  }
 }
 
 void __nesc_enable_interrupt(void) @safe() {
-  if (interrupts_enabled) return;
-  sigprocmask(SIG_SETMASK, &global_sig, NULL);
-  interrupts_enabled = TRUE;
+  // To re-enable interrupts, just apply the old sigmask we had before we
+  // disabled interrupts.
+  interupts_enabled = TRUE;
+  sigprocmask(SIG_SETMASK, &global_sigmask, NULL);
 }
 
-/* @spontaneous() functions should not be included when NESC_BUILD_BINARY
-   is #defined, to avoid duplicate functions definitions when binary
-   components are used. Such functions do need a prototype in all cases,
-   though. */
 __nesc_atomic_t __nesc_atomic_start(void) @spontaneous() @safe() {
+  __nesc_atomic_t prev_state = interupts_enabled;
   __nesc_disable_interrupt();
-  return 1;
+  return prev_state;
 }
 
 void __nesc_atomic_end(__nesc_atomic_t reenable_interrupts) @spontaneous() @safe() {
-  __nesc_enable_interrupt();
+  if (reenable_interrupts) {
+    __nesc_enable_interrupt();
+  }
 }
 
-
-
 #endif
-
-
