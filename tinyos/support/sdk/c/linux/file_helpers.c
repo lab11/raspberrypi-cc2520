930342b62d3d332cc5a71b0ee83074870ff3cd8e
Brad Campbell <bradjc5@gmail.com>
2013-10-24 23:47:53 -0400

    fixed indent

--- tinyos/support/sdk/c/linux/file_helpers.c
+++ tinyos/support/sdk/c/linux/file_helpers.c
@@ -6,16 +6,16 @@
 // Makes the given file descriptor non-blocking.
 // Returns 1 on success, 0 on failure.
 int make_nonblocking (int fd) {
-int flags, ret;
+  int flags, ret;
 
-flags = fcntl(fd, F_GETFL, 0);
-if (flags == -1) {
-  return 0;
-}
-// Set the nonblocking flag.
-flags |= O_NONBLOCK;
-ret = fcntl(fd, F_SETFL, flags);
+  flags = fcntl(fd, F_GETFL, 0);
+  if (flags == -1) {
+    return 0;
+  }
+  // Set the nonblocking flag.
+  flags |= O_NONBLOCK;
+  ret = fcntl(fd, F_SETFL, flags);
 
-return ret != -1;
+  return ret != -1;
 }
 
