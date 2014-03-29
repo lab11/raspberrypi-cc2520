16383352073f86eda9fe8603e5ea8bdd2578e01c
William Huang <wwhuang@umich.edu>
2014-03-29 15:35:00 -0400

    fixing the way ip is determined

--- utils/identify_rpi.py
+++ utils/identify_rpi.py
@@ -2,7 +2,7 @@ import requests, time
 from uuid import getnode as get_mac
 import socket
 
-POST_URL = 'https://woc94o08ipz0.runscope.net'
+POST_URL = 'https://www.runscope.com/stream/gbns1w57rtvk'
 
 for i in range(0,10):
 
@@ -11,7 +11,9 @@ for i in range(0,10):
 	mactem = '{:0>12x}'.format(macraw)
 	macfmt = ':'.join([mactem[i:i+2] for i in range(0, 12, 2)])
 
-	ipaddr = socket.gethostbyname(socket.gethostname())
+	s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(('www.google.com', 0))
+    ipaddr = s.getsockname()[0]
 
 	r = requests.post(POST_URL,
 		data={"time":time.time(), "macaddr":macfmt, "ipaddr":ipaddr})
