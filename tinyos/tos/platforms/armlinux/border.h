eb14a88b9318b295e69e7da0a8fbf83e6a053a22
Brad Campbell <bradjc5@gmail.com>
2014-12-23 21:44:24 -0500

    rcombine comment

--- tinyos/tos/platforms/armlinux/border.h
+++ tinyos/tos/platforms/armlinux/border.h
@@ -5,4 +5,11 @@ enum {
   ROUTE_IFACE_TUN = 3,
 };
 
+// Might need to do this.
+// typedef uint8_t bool __attribute__((combine(rcombine)));
+
+// bool rcombine(bool a, bool b) {
+// 	return a & b;
+// }
+
 #endif
